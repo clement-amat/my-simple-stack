177b4e26c20ae798269c7cbe8ce028f22252357c
clement-amat <clemamat@gmail.com>
2018-10-18 11:18:50 +0200

    test: modifications diverses

---
+++ src/main/java/deqo/cama/mysimplestack/App.java
@@ -0,0 +1,15 @@
+package deqo.cama.mysimplestack;
+
+/**
+ * Hello world application
+ *
+ */
+public class App {
+    /**
+     * Programme bonjour
+     * @param args parametres
+     */
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
