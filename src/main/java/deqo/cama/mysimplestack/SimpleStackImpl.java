501c216089eeb44951a4913cb2697c12cd2f974c
clement-amat <clemamat@gmail.com>
2018-10-25 10:21:12 +0200

    Implementer toString() progress

--- src/main/java/deqo/cama/mysimplestack/SimpleStackImpl.java
+++ src/main/java/deqo/cama/mysimplestack/SimpleStackImpl.java
@@ -52,4 +52,12 @@ public class SimpleStackImpl implements SimpleStack {
         size --;
         return toPop;
     }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = getSize() - 1 ; i >= 0 ; i -- ) {
+            sb.append(stack[i].getValue().toString() + " ");
+        }
+        return sb.toString();
+    }
 }
