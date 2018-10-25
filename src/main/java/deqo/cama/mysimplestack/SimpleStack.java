d3c78725686a27bcccded836aad6bb19ca63ed56
clement-amat <clemamat@gmail.com>
2018-10-25 10:49:27 +0200

    my-simple-stack-6 Mise a jour de l'interface

--- src/main/java/deqo/cama/mysimplestack/SimpleStack.java
+++ src/main/java/deqo/cama/mysimplestack/SimpleStack.java
@@ -6,6 +6,7 @@ public interface SimpleStack {
     public boolean isEmpty();
     public int getSize();
     public void push(Item item);
+    public void push(Item ... item);
     public Item peek() throws EmptyStackException;
     public Item pop() throws EmptyStackException;
 }
